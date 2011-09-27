6959aba3c899de2ae416c9ce8699ccdd21dd52cd
Simon Moon <simoon@gmx.com>
2011-09-27 18:30:24 -0400

    Added chat message sanitation

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 var io = require('socket.io').listen(4444);
 
 //Initial variables for start of node
-last_coords = {'mouseX': 133, 'mouseY': 133}
-last_msg = { 'name':'derpy', 'text': 'I love muffins~'}
-ip_list = []
+last_coords = {'mouseX': 133, 'mouseY': 133};
+last_msg = { 'name':'derpy', 'text': 'I love muffins~'};
+ip_list = [];
+
+function sanitize(text){
+  var i;
+  var clean_text = '';
+  var strip = '<>';
+  for(i = 0; i < text.length; i++){
+    var c = text.charAt(i);
+    if(strip.indexOf(c) == -1) clean_text += c;
+  }
+  return clean_text;
+}
 
 io.sockets.on('connection', function (socket) {
  socket.emit('join', last_coords); 
@@ -21,8 +32,8 @@ io.sockets.on('connection', function (socket) {
  });
 
  socket.on('message', function(data) {
-   last_msg = data;
-   console.log(data);
-   socket.broadcast.emit('message',data);
+   var clean_data = {'name':sanitize(data.name), 'text': sanitize(data.text)};
+   var last_msg = clean_data;
+   socket.broadcast.emit('message', clean_data);
  });
 });
