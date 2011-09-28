44b30b744dd7f294f813c1ae328b5dcbd31a60e3
Simon Moon <simoon@gmx.com>
2011-09-28 03:06:17 -0400

    Multi-Derpy support, escape HTML... etc!

--- pony.js
+++ pony.js
@@ -1,69 +1,93 @@
-function create_avatar(x,y){
-    $('#flyzone').append('<img id=\'avatar\'src=\'derpy_hover_right.gif\'/>');
-    $('#flyzone').append('<span id=\'chat_display\'></span>');
-    $('#avatar').css({'left': x - 50 ,'top': y - 50});
-    $('#chat_display').css({'left': x + 50 ,'top': y - 100});
+function create_avatar(id,x,y){
+    $('#flyzone').append('<img class=\'avatar\' id=\''+ id +'\'src=\'derpy_hover_right.gif\'/>');
+    $('#flyzone').append('<span class=\'chat_display\' id=\'bubble'+ id +'\'></span>');
+    $('#'+id+'.avatar').css({'left': x - 55 ,'top': y - 55});
+    $('#bubble'+id+'.chat_display').css({'left': x + 25 ,'top': y - 50});
   }
 
-  function animate(x,y){  //animate avatar function
+function animate(id,x,y){  //animate avatar function
     var x = x - 50
     var y = y - 50
-    $('#chat_display').animate({'left': x + 100,  //animate chat text
-                                'top': y - 50 },'slow');
-    av_position = $('#avatar').position()
-    if (av_position.left < x){  //change direction of pony if needed
-      $('#avatar').attr('src','derpy_fly_right.gif');
+    var avatar_select = '#'+id+'.avatar';
+    $('#bubble'+id+'.chat_display').animate({'left': x + 70,  //animate chat text
+                                'top': y - 20 },'slow');
+    
+    //change direction of pony if needed
+    av_position = $(avatar_select).position()
+    if (av_position.left < x){
+      $(avatar_select).attr('src','derpy_fly_right.gif');
       var right = 1;
     } else {
-      $('#avatar').attr('src','derpy_fly_left.gif');
+      $(avatar_select).attr('src','derpy_fly_left.gif');
       var right = 0;
     }
 
-    $('#avatar').animate({'left': x, //animate w/func to hover right or left
-                          'top': y },'slow', function() {
+    $(avatar_select).animate({'left': x, //animate w/func to hover right or left
+                              'top': y },'slow', function() {
       if (right == 1){
-        $('#avatar').attr('src','derpy_hover_right.gif');
+        $(avatar_select).attr('src','derpy_hover_right.gif');
       } else {
-        $('#avatar').attr('src','derpy_hover_left.gif');
+        $(avatar_select).attr('src','derpy_hover_left.gif');
       }
     });
   }
 
-  function display_msg(name,msg) { //function to display new messages
-    $('#chat_display').append(name + ':'+ msg + '<br/>');
+  function display_msg(id,name,msg) { //function to display new messages
+    $('#bubble'+id+'.chat_display').append(name + ':'+ msg + '<br/>');
+  }
+  
+  function display_connected(pony_array){
+    for(i in pony_array){
+      var rand = Math.round(Math.random()*444)+50;
+      create_avatar(pony_array[i],rand,rand);
+    }
   }
 
   var socket = io.connect('http://fgsfds.com:4444'); //socket.io recieve setup
 
-  socket.on('join', function(data) {
-    create_avatar(data.mouseX, data.mouseY);
+  socket.on('my_join', function(data){
+    display_connected(data.list);
+    my_pony_id = data.id;
+    $('#flyzone').append(my_pony_id + ' welcome!!<br/>')
   });
-  socket.on('click', function (data) {
-    animate(data.mouseX,data.mouseY); //animate avatar
+
+  socket.on('join', function(data) {
+    var pony_id = data.id;
+    var rand = Math.round(Math.random()*444)+50;
+    $('#flyzone').append(pony_id + ' has joined us<br/>')
+    create_avatar(pony_id,rand,rand);
+  });
+
+  socket.on('leave', function(data){
+    $('#'+data.id).remove();
+    $('#bubble'+data.id).remove();
+    $('#flyzone').append(data.id + ' has left us<br/>')
+  });
+  socket.on('click', function(data) {
+    animate(data.id, data.mouseX, data.mouseY); //animate others avatars
   });
   socket.on('message',function(data) {
-    display_msg(data.name, data.text); //display your chat msgs
+    display_msg(data.id, data.name, data.text); //display your chat msgs
   });
 
   $(document).ready(function(){ 
     chat_name = $('#name').attr('value');
 
-    $(document).click(function(){ //focus to chatbox always
-      //$('#text_entry').focus();
-    });
-
     $('#flyzone').click(function(e){  //mouse clicks socket.io and animate
-      socket.emit('click', { 'mouseX': e.pageX, 'mouseY': e.pageY });
-      animate(e.pageX,e.pageY);
+      socket.emit('click', { 'id': my_pony_id,
+                             'mouseX': e.pageX,
+                             'mouseY': e.pageY });
+      animate(my_pony_id, e.pageX, e.pageY); //animate my avatar
       $('#text_entry').focus();
     });
+
     $('#text_entry').focus();
     $('#text_entry').keydown(function(event){  //on 'enter' do stuff
     if(event.keyCode == '13'){
         var chat_name = $('#name').attr('value');
         var chat_send = $('#text_entry').attr('value');
-        socket.emit('message', {'name': chat_name, 'text': chat_send});
-        display_msg(chat_name, chat_send); //display your chat msgs
+        socket.emit('message', {'id': my_pony_id, 'name': chat_name, 'text': chat_send});
+        display_msg(my_pony_id, chat_name, chat_send); //display your chat msgs
         $('#text_entry').attr('value',''); //clear texty, and refocus
         $('#text_entry').focus();
       };
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
+var request = require('request');
 var io = require('socket.io').listen(4444);
-
-//Initial variables for start of node
-last_coords = {'mouseX': 133, 'mouseY': 133};
-last_msg = { 'name':'derpy', 'text': 'I love muffins~'};
-ip_list = [];
+pony_array = []
 
 function sanitize(text){
   var i;
@@ -12,27 +9,43 @@ function sanitize(text){
   for(i = 0; i < text.length; i++){
     var c = text.charAt(i);
     if(strip.indexOf(c) == -1) clean_text += c;
+    if(strip.indexOf(c) == 0) clean_text += '&lt;';
+    if(strip.indexOf(c) == 1) clean_text += '&gt;';
   }
   return clean_text;
 }
 
-io.sockets.on('connection', function (socket) {
- socket.emit('join', last_coords); 
- socket.emit('message', last_msg); //last message on connect 
 
- var address = socket.handshake.address;
- ip_list.push(address.address);
- console.log("Client IPs: " + ip_list);
- console.log(socket.id);
- 
+io.sockets.on('connection', function(socket) {
+ pony_array.push(socket.id);
+ rand_start = Math.round(Math.random()*444)+50;
+
+ socket.emit('my_join', { 'id': socket.id,
+                          'list': pony_array, 
+                          'mouseX': rand_start,
+                          'mouseY': rand_start });
+
+ socket.broadcast.emit('join', { 'id': socket.id, 
+                                 'mouseX': rand_start,
+                                 'mouseY': rand_start });
+
+ socket.on('disconnect', function() {
+   index = pony_array.indexOf(socket.id);
+   if(index != -1) pony_array.splice(index, 1);
+   socket.broadcast.emit('leave', {'id':socket.id})
+ });
+
+/* var address = socket.handshake.address;
+ console.log("Client IP: " + address.address);
+*/ 
  socket.on('click', function(data) {
    last_coords = data;
-   console.log(data);
    socket.broadcast.emit('click',data);
  });
 
  socket.on('message', function(data) {
-   var clean_data = {'name':sanitize(data.name), 'text': sanitize(data.text)};
+   var clean_data = { 'id': data.id, 'name': sanitize(data.name),
+                      'text': sanitize(data.text)};
    var last_msg = clean_data;
    socket.broadcast.emit('message', clean_data);
  });
